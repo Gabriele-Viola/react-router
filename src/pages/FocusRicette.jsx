103c1af7e921e5e1212d5c4e62121be1cf18c09a
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-26 13:50:57 +0100

    fix FocusRicette

--- src/pages/FocusRicette.jsx
+++ src/pages/FocusRicette.jsx
@@ -14,13 +14,38 @@ export default function FocusRicetta() {
                 .then(res => res.json())
                 .then(data => {
                     console.log(data);
+                    const keys = Object.keys(data)
+                    console.log(keys);
+                    if (keys.includes('error')) {
+                        navigate('/404')
+
+                    } else {
+                        setRicetta(data.data)
+                    }
 
                 })
-        }
-    )
+                .catch(err => {
+                    console.error(err);
+
+                })
+        }, [])
     return (
         <>
 
+            {ricetta ? (
+
+                <div className="container">
+                    <div className="card">
+                        <img src={`http://localhoste:3000/imgs/${ricetta.image}`} alt={ricetta.title} />
+                        <h3>{ricetta.title}</h3>
+                        <div className="description">
+                            {ricetta.content}
+                        </div>
+                    </div>
+                </div>
+            ) : (<div>loading...</div>)
+
+            }
 
 
 
