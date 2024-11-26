d13c72dda4a646f51c616569b793a612a2a12139
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-11-26 17:06:16 +0100

    add new slug

--- src/pages/FocusRicette.jsx
+++ src/pages/FocusRicette.jsx
@@ -1,51 +1,52 @@
-import { useParams, useNavigate } from "react-router"
+import { useParams, useNavigate, Link } from "react-router"
 import { useEffect, useState } from "react"
 
 export default function FocusRicetta() {
     const navigate = useNavigate()
     const [ricetta, setRicetta] = useState(null)
+    const [next, setNext] = useState(null)
     const [ricette, setRicette] = useState(null)
     const [index, setIndex] = useState('')
     const { slug } = useParams()
-    const urlSlug = `http://localhoste:3000/ricette/${slug}`
+    // const urlSlug = `http://localhoste:3000/ricette/${slug}`
     const url = 'http://localhoste:3000/ricette/'
-    console.log(urlSlug);
+    console.log(url);
 
     useEffect(
         () => {
             fetch(url)
                 .then(res => res.json())
                 .then(data => {
-                    console.log(data);
-                    const keys = Object.keys(data)
-                    console.log(keys);
-                    if (keys.includes('error')) {
-                        navigate('/404')
+                    console.log(data, slug);
+                    console.log(data.data[2]);
 
-                    } else {
-                        const allRicette = data.data
-                        setRicette(allRicette)
 
-                    }
-                    const singleRicetta = ricette.find(ricetta => ricetta.slug == slug)
-                    setRicetta(singleRicetta)
-                    const indx = ricette.indexOf(ricetta)
-                    setIndex(indx)
+                    setRicetta(data.data.find(ricetta => ricetta.slug === slug))
+                    setIndex(data.data.findIndex(ricetta => ricetta.slug === slug))
+
+                    setNext(data.data[index + 1].slug)
+
                 })
                 .catch(err => {
                     console.error(err);
 
                 })
+
         }, [])
+
+
+
     return (
         <>
 
             {ricetta ? (
 
                 <div className="container">
+                    <Link to=''>prev</Link>
+
                     <div className="card">
                         <img src={`http://localhoste:3000/imgs/${ricetta.image}`} alt={ricetta.title} />
-                        <h3>{ricetta.title}{index}</h3>
+                        <h3>{ricetta.title}</h3>
                         <div className="description">
                             {ricetta.content}
                         </div>
@@ -53,6 +54,7 @@ export default function FocusRicetta() {
                             {ricetta.tags.map((tag, index) => <div key={index} className='tag' >{tag}</div>)}
                         </div>
                     </div>
+                    <Link to={`/ricette/${next}`}>next</Link>
                     <div className="story">
                         Il ciambellone è un dolce tradizionale le cui origini affondano nelle radici della cucina popolare europea. Sebbene oggi sia diffuso in molte varianti, la sua storia inizia come preparazione semplice e rustica, nata dalla necessità di sfruttare ingredienti facilmente reperibili in ambito domestico. Il nome "ciambellone" deriva da "ciambella", un termine che si riferisce alla forma circolare caratteristica, con o senza foro centrale, e richiama antichi dolci preparati già in epoca romana. Durante il Medioevo, in molte regioni d'Europa, erano comuni dolci dalla forma circolare, spesso cotti in forni comuni del villaggio. La forma ad anello aveva anche un valore simbolico: rappresentava l'unità e la continuità. Nel tempo, il ciambellone è diventato un simbolo della cucina casalinga, grazie alla sua semplicità. Gli ingredienti di base – farina, zucchero, uova, burro (o olio) e lievito – erano facilmente reperibili, mentre l'aggiunta di aromi come limone, vaniglia, o liquori variava in base alla disponibilità locale. Il ciambellone veniva preparato soprattutto per le colazioni o come merenda, legato alle tradizioni contadine e alle famiglie numerose. Oggi il ciambellone ha assunto numerose varianti regionali in Italia e nel mondo, dalle versioni arricchite con cacao, uvetta o frutta secca a quelle più moderne senza glutine o lattosio. Nonostante l'evoluzione delle ricette, rimane un dolce che evoca il calore familiare e la semplicità della tradizione, portando avanti il gusto genuino della cucina di un tempo.
                     </div>
